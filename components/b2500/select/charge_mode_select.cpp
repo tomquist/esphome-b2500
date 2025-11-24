16e56eca466c3d8bb8defb80f7840076a4568f58
Tom Quist <tomquist@users.noreply.github.com>
2025-11-24 21:18:45 +0100

    Fix charge mode select for ESPHome 2025.11.0

--- components/b2500/select/charge_mode_select.cpp
+++ components/b2500/select/charge_mode_select.cpp
@@ -6,7 +6,12 @@ namespace b2500 {
 
 void ChargeModeSelect::setup() {
   auto charge_modes = this->parent_->get_valid_charge_modes();
-  this->traits.set_options(charge_modes);
+  FixedVector<const char *> options;
+  options.init(charge_modes.size());
+  for (auto &mode : charge_modes) {
+    options.push_back(mode.c_str());
+  }
+  this->traits.set_options(options);
   this->parent_->get_state()->add_on_message_callback([this](B2500Message message) {
     if (message == B2500_MSG_RUNTIME_INFO) {
       auto charge_mode = this->parent_->get_charge_mode();
