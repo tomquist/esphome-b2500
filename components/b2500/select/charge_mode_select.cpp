379cbbe5a895542991b3e1d96bfb88edc5c49104
Tom Quist <tomquist@users.noreply.github.com>
2025-11-24 22:03:59 +0100

    Ensure charge mode options use compile-time literals

--- components/b2500/select/charge_mode_select.cpp
+++ components/b2500/select/charge_mode_select.cpp
@@ -1,17 +1,21 @@
 #include "charge_mode_select.h"
 #include "../b2500_state.h"
 
+#include <algorithm>
+
 namespace esphome {
 namespace b2500 {
 
 void ChargeModeSelect::setup() {
+  constexpr const char *CHARGE_MODE_OPTIONS_V1[] = {"LoadFirst", "PV2Passthrough"};
+  constexpr const char *CHARGE_MODE_OPTIONS_V2[] = {"LoadFirst", "SimultaneousChargeAndDischarge"};
+
   auto charge_modes = this->parent_->get_valid_charge_modes();
-  FixedVector<const char *> options;
-  options.init(charge_modes.size());
-  for (auto &mode : charge_modes) {
-    options.push_back(mode.c_str());
+  if (std::find(charge_modes.begin(), charge_modes.end(), "SimultaneousChargeAndDischarge") != charge_modes.end()) {
+    this->traits.set_options({CHARGE_MODE_OPTIONS_V2[0], CHARGE_MODE_OPTIONS_V2[1]});
+  } else {
+    this->traits.set_options({CHARGE_MODE_OPTIONS_V1[0], CHARGE_MODE_OPTIONS_V1[1]});
   }
-  this->traits.set_options(options);
   this->parent_->get_state()->add_on_message_callback([this](B2500Message message) {
     if (message == B2500_MSG_RUNTIME_INFO) {
       auto charge_mode = this->parent_->get_charge_mode();
